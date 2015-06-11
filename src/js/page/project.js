d7647b70414b72ab88ea429ad1c54e690b636833
soulteary <soulteary@gmail.com>
2015-06-11 14:43:20 +0800

    update, todo:add tpl

--- src/js/page/project.js
+++ src/js/page/project.js
@@ -1,22 +1,49 @@
 /**
  * 项目页面
  */
-define(function(require, module, exports) {
+define(function (require, module, exports) {
     'use strict';
 
     var $ = window.$;
     var projectPage = $('.js-page-project');
 
+    /**
+     * 获取所有项目列表
+     */
+    function getList() {
+        if (projectPage.length) {
+            $.post('/project-list', null, function (response) {
+                if (response) {
+                    switch (response.code) {
+                        case 200:
+                            console.log(response.data);
+                            break;
+                        case 400:
+                            console.log(response.data);
+                            break;
+                    }
+                }
+            });
+        }
+    }
+
+
+    function pageLoaded() {
+        getList();
+    }
+
+
     function init() {
         if (projectPage.length) {
-            projectPage.find('.js-create-project-button').on('click', function(e) {
+            projectPage.find('.js-create-project-button').on('click', function (e) {
                 e.preventDefault();
                 e.stopPropagation();
-                $.post('/create-project', {'domain':projectPage.find('.input-domain').val()}, function(resp){
+                $.post('/create-project', {'domain' : projectPage.find('.input-domain').val()}, function (resp) {
                     console.log(resp);
                 });
             });
         }
+        pageLoaded();
     }
 
     return {
